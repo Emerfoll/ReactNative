57cbc018c893fb950e43d806ddfffb8cb61b7597
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-05-01 18:21:42 -0500

    More native fun

--- App.js
+++ App.js
@@ -1,56 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground, Image, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+  Image,
+  Alert,
+  Button,
+} from "react-native";
 
 const image = { uri: "https://www.enjpg.com/img/2020/4k-mobile-7.jpg" };
 // const image = { uri: "https://www.enjpg.com/img/2020/4k-for-mobile-3.jpg" };
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ImageBackground source={image} style={styles.background}>
-      <Text style={styles.text}>My first React Native app!</Text>
-      <StatusBar style="auto" />
-      
-      {/* <Image 
+        <View style={{
+          // backgroundColor: 'purple',
+          width: "75%",
+          alignItems: 'center',
+          marginHorizontal: 55,
+          
+        }}>
+          <Text style={styles.text}>My first React Native app!</Text>
+          <StatusBar style="auto" />
+
+          {/* <Image 
       source={{ 
         width: 200,
         height: 300,
         uri: "https://i.picsum.photos/id/397/200/300.jpg?hmac=9VBInLrifj_yyc2JuJSAVIfj9yQdt5Ovm2sHmvva-48"}} 
       /> */}
 
-      <Text style={styles.text2} onPress={() => console.log('name clicked')}>By: Chris Emerfoll</Text>
+          <Text
+            style={styles.text2}
+            onPress={() => console.log("name clicked")}
+          >
+            By: Chris Emerfoll
+          </Text>
 
-      <View style={styles.button}>
-        <Button 
-         
-        title='View Portfolio' onPress={() => console.log('button pressed')} 
-        accessibilityLabel="Blue View Portfolio Button"
-        />
-      </View>
-
+          <View style={styles.button}>
+            <Button
+              title="View Portfolio"
+              onPress={() =>
+                Alert.alert("Title", "Alert Message", [
+                  { text: "yes", onPress: () => console.log("yes clicked") },
+                  { text: "no", onPress: () => console.log("no clicked") },
+                ])
+              }
+              accessibilityLabel="Blue View Portfolio Button"
+            />
+          </View>
+        </View>
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'gray',
+    flexDirection: "column",
+    backgroundColor: "gray",
     // alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   button: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginTop: 25,
   },
   background: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   text: {
     color: "white",
@@ -65,8 +91,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     // fontWeight: "bold",
     textAlign: "center",
-    
-    backgroundColor: "#000000a0"
-    
-  }
+
+    backgroundColor: "#000000a0",
+  },
 });
